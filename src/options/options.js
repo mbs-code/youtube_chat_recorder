2a9329752ce34597a70db003a6552130dedd6050
mabasasi <aoe317293@yahoo.co.jp>
2020-12-30 11:36:52 +0900

    [add] option に browser の追加(忘れ)

--- src/options/options.js
+++ src/options/options.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App'
 
-global.browser = require('webextension-polyfill')
+global.browser = require('webextension-polyfill-ts')
+Vue.prototype.$browser = global.browser
 
 /* eslint-disable no-new */
 new Vue({
